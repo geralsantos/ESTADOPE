2723a056026ef4121fbb5c7e6ed33559c767b8a4
Emer Isau <emer.isau@gmail.com>
2020-04-12 16:51:37 -0500

    Se ha creado el splash creen y añadido dependencias

---
+++ lib/config.dart
@@ -0,0 +1,5 @@
+const ROOT="http://138.68.42.71/estadope/api";
+const APP_TITLE="MY APP";
+//http://138.68.42.71/estadope/api/registros/tipodocumento/ver/
+//http://138.68.42.71/estadope/api/registros/tipocaptura/ver/
+//http://138.68.42.71/estadope/api/login/supervisor/
